45e84123bfcf0129fdaf2f22a43d9ba53a40bda1
Joel Smith <joelsmith2004@gmail.com>
2023-04-20 10:15:07 +0000

    20 Apr 2023, 18:15

--- brushfire.cpp
+++ brushfire.cpp
@@ -1,4 +1,45 @@
 //
 #include "eyebot++.h"
+#include <vector>
+using std::vector;
 
+typedef struct Pixel{
+    int x; 
+    int y;
+    int id;
+    int expansion = 0;
+    int value;
+    bool voroni = false;
+    vector<int> allObjectId;
+} Pixel;
 
+struct Object{
+    int id;
+    vector<Pixel> allPixels;
+
+    void addPixeltoLine(Pixel pixel){
+        allPixels.push_back(pixel);
+    }
+
+    
+} Object;
+
+struct Walls{
+    vector<Pixel> top;
+    vector<Pixel> bottom;
+    vector<Pixel> left;
+    vector<Pixel> right;
+
+    void addPixeltoTop(Pixel pixel){
+        top.push_back(pixel);
+    }
+    void addPixeltoBottom(Pixel pixel){
+        bottom.push_back(pixel);
+    }
+    void addPixeltoLeft(Pixel pixel){
+        left.push_back(pixel);
+    }
+    void addPixeltoTop(Pixel pixel){
+        right.push_back(pixel);
+    }
+} Walls;
