fc5d6a3d6ed3afe0b86324bd14cb017b85480c7f
Joel Smith <joelsmith2004@gmail.com>
2023-04-26 06:49:49 +0000

    26 Apr 2023, 14:49

--- brushfire.cpp
+++ brushfire.cpp
@@ -296,7 +296,7 @@ void groupPixel()
         for (int j = 0; j < 128; j++) {
 
 
-            if (image[i * 128+j]){
+            if (image[i * 128+j && j < 127]){
                 
                 
 
